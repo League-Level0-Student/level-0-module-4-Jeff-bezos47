e079ddd3c4505abe7a1f433daf3d9de362b90795
jeff <jeff@localhost>
2025-12-21 12:57:27 -0800

    dfasdf

--- src/_02_boolean/_3_game_over/GameOver.java
+++ src/_02_boolean/_3_game_over/GameOver.java
@@ -6,19 +6,20 @@ public class GameOver {
 	
 	public static void main(String[] args) {
 
-		boolean gameOver = false;
-		
+		boolean gameOver = true;
+	while(gameOver = false) {
 		// 1. Ask the user if the game is over and save their answer.
 		String game = JOptionPane.showInputDialog("is you game over");
 		// 2. If they answer "yes" change gameOver to true.
 		if(game.equals("yes")) {
 			gameOver = true;
 		}
+	}
 		// 3. Surround steps 1-2 in a while loop that runs as long as
 		//    gameOver is false. Hint: Use the ! operator.
 		
 		// 4. Tell the user the game is over outside the while loop.
-		
+	JOptionPane.showMessageDialog(null, "game over");
 	}
 
 }
